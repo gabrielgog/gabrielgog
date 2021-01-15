357742f9a8e3ef34525eb2a271bd109a84e99429
Gabriel Godwin <gabrielgog96@gmail.com>
2021-01-15 18:40:38 +0100

    add about home

--- src/body_sec.jsx
+++ src/body_sec.jsx
@@ -9,9 +9,29 @@ function body(){
 <div className="me">
 
 
-    <h1>Gabriel Godwin</h1>
+    <img className="homeimg" src="/gabriel_gog.png"/>
+    <h3 className="heading">Gabriel Godwin </h3>
 
-    <p>Software Developer</p>
+    <p className="paragraph">Software Engineer</p>
+
+    <p className="home-about">Gabriel Godwin is a software engineer that focuses on JavaScript and 
+        the React Framework. He has a passion for Design systems, Data Science and 
+        serverless technologies such as Amazon Web services and the Google cloud platform <a href="">....Read More</a>
+    </p>
+
+
+ <div className="social">
+
+ <a href=""><img src="/linkedin.png"/></a>
+<a href=""><img src="/medium.png"/></a>
+<a href=""><img src="/twitter.png"/></a>
+<a href=""><img src="/github.png"/></a>
+
+
+
+
+ </div>
+
 
 
 
@@ -20,7 +40,12 @@ function body(){
 
 
 
+
+
+
     </div>
+
+
 
 
 
